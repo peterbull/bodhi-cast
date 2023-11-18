75e1210b9145578f091f3b76cbb42ff97523e8b2
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-18 08:39:28 -0500

    update model schema

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,10 +1,9 @@
-from sqlalchemy import Column, Float, Integer, String, ARRAY, JSON
+from sqlalchemy import Column, Float, Integer, String, JSON, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
-class SurfData(Base):
+class SwellData(Base):
     __tablename__ = 'surf_data'
     id = Column(Integer, primary_key=True)
     latitude = Column(Float)
@@ -15,6 +14,6 @@ class SurfData(Base):
     timezone_abbreviation = Column(String)
     elevation = Column(Integer)
     hourly_units = Column(JSON)
-    hourly = Column(Float)
-    swell_wave_height = Column(Float)  # Assuming array of float values
-    swell_wave_period = Column(Float)  # Assuming array of float values
+    hourly = Column(DateTime(timezone=True))
+    swell_wave_height = Column(Float)
+    swell_wave_period = Column(Float)
