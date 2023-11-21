6a0b64be0be04236f2a8673effe039a59aa9a1d6
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-21 11:04:43 -0500

    add boilerplate react-leaflet map

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,55 +1,28 @@
+import React from 'react';
 import { Fragment } from 'react';
 import './App.css';
-import cladioPic from './imgs/IMG_2706.jpg';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const Button = ({ children }) => (
-  <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-    {children}
-  </button>
-);
-
-const Alert = ({ message, type}) => (
-  <div className={`p-4 ${type === 'error' ? 'bg-red-100' : 'bg-green-100'}`}>
-    {message}
-  </div>
-);
-
-const toggleButton = () => {
-  const [isActive, setIsActive] = useState(false);
-
+const MyMap = () => {
   return (
-    <button
-      onClick={() => setIsActive(!isActive)}
-  )
-}
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
 function App() {
   return (
     <Fragment>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg p-4">
-        <img className="w-full" src={cladioPic} alt="User pic" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">John Doe</div>
-          <p className="text-gray-700 text-base">
-            Some quick example text to build on the card and make up the bulk of the card's content.
-          </p>
-        </div>
-      </div>
-    
-      <Button>
-        Click Me
-      </Button>
-
-      <Alert message="Success" type="success" />
-      <Alert message="Error" type="error" />
-    
-      {/* responsive design, md: lg: */}
-      <div className='flex flex-col md:flex-row text-center'>
-        <div className='md:w-1/2'>Left Side</div>
-        <div className='md:w-1/2'>Right Side</div>
-      </div>
-
-
+      <MyMap /> 
     </Fragment>
   );
 }
