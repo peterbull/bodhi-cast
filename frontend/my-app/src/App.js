50affa05569f6faf619e78adb3fb59767619283b
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-21 10:42:02 -0500

    end of testing tailwind

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -2,6 +2,27 @@ import { Fragment } from 'react';
 import './App.css';
 import cladioPic from './imgs/IMG_2706.jpg';
 
+const Button = ({ children }) => (
+  <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+    {children}
+  </button>
+);
+
+const Alert = ({ message, type}) => (
+  <div className={`p-4 ${type === 'error' ? 'bg-red-100' : 'bg-green-100'}`}>
+    {message}
+  </div>
+);
+
+const toggleButton = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <button
+      onClick={() => setIsActive(!isActive)}
+  )
+}
+
 function App() {
   return (
     <Fragment>
@@ -14,8 +35,21 @@ function App() {
           </p>
         </div>
       </div>
+    
+      <Button>
+        Click Me
+      </Button>
 
-      
+      <Alert message="Success" type="success" />
+      <Alert message="Error" type="error" />
+    
+      {/* responsive design, md: lg: */}
+      <div className='flex flex-col md:flex-row text-center'>
+        <div className='md:w-1/2'>Left Side</div>
+        <div className='md:w-1/2'>Right Side</div>
+      </div>
+
+
     </Fragment>
   );
 }
