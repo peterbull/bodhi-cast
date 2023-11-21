6d2e04b69c7c354671cc01be57c1b0b15b81cff5
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-21 09:49:39 -0500

    add tailwind example

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
 import './App.css';
+import cladioPic from './imgs/IMG_2706.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg p-4">
+        <img className="w-full" src={cladioPic} alt="User pic" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">John Doe</div>
+          <p className="text-gray-700 text-base">
+            Some quick example text to build on the card and make up the bulk of the card's content.
+          </p>
+        </div>
+      </div>
+
+      
+    </Fragment>
   );
 }
 
