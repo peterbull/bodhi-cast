1e7021072bcdf91451bc4766b0c3be17daab52a1
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-26 14:09:59 -0500

    fix unnecessary 'response'

--- backend/app/utils/fetch_data.py
+++ backend/app/utils/fetch_data.py
@@ -20,23 +20,23 @@ def fetch_data():
     return res.json()
 
 def parse_swell_data(json_data):
-    hourly_data = json_data.requests.get('hourly', {})
-    times = hourly_data.requests.get('time', [])
-    heights = hourly_data.requests.get('swell_wave_height', [])
-    periods = hourly_data.requests.get('swell_wave_period', [])
+    hourly_data = json_data.get('hourly', {})
+    times = hourly_data.get('time', [])
+    heights = hourly_data.get('swell_wave_height', [])
+    periods = hourly_data.get('swell_wave_period', [])
 
     swell_data_list = []
 
     for time, height, period in zip(times, heights, periods):
         swell_data = SwellData(
-                    latitude=json_data.requests.get('latitude'),
-                    longitude=json_data.requests.get('longitude'),
-                    generationtime_ms=json_data.requests.get('generationtime_ms'),
-                    utc_offset_seconds=json_data.requests.get('utc_offset_seconds'),
-                    timezone=json_data.requests.get('timezone'),
-                    timezone_abbreviation=json_data.requests.get('timezone_abbreviation'),
-                    elevation=json_data.requests.get('elevation'),
-                    hourly_units=json_data.requests.get('hourly_units'),
+                    latitude=json_data.get('latitude'),
+                    longitude=json_data.get('longitude'),
+                    generationtime_ms=json_data.get('generationtime_ms'),
+                    utc_offset_seconds=json_data.get('utc_offset_seconds'),
+                    timezone=json_data.get('timezone'),
+                    timezone_abbreviation=json_data.get('timezone_abbreviation'),
+                    elevation=json_data.get('elevation'),
+                    hourly_units=json_data.get('hourly_units'),
                     hourly=time,
                     swell_wave_height=height,
                     swell_wave_period=period
