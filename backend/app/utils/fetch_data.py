df38b972ad195020722084ac6de16d87c903df5f
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-26 09:06:07 -0500

    add fetch and write funcs for grib files

--- backend/app/utils/fetch_data.py
+++ backend/app/utils/fetch_data.py
@@ -1,30 +1,42 @@
-from requests import get
+import requests
+import re
+import tempfile
+import xarray as xr
+from datetime import datetime
+import pytz
+
+from bs4 import BeautifulSoup
+from sqlalchemy.exc import SQLAlchemyError
+
+
 from app.models.models import SwellData
 from app.db.database import SessionLocal
 
+### Open Meteo Hourly Swell Data for Va Beach
+
 def fetch_data():
     url = "https://marine-api.open-meteo.com/v1/marine?latitude=36.83088925769251&longitude=-75.96727139259976&hourly=swell_wave_height,swell_wave_period"
-    res = get(url)
+    res = requests.get(url)
     return res.json()
 
 def parse_swell_data(json_data):
-    hourly_data = json_data.get('hourly', {})
-    times = hourly_data.get('time', [])
-    heights = hourly_data.get('swell_wave_height', [])
-    periods = hourly_data.get('swell_wave_period', [])
+    hourly_data = json_data.requests.get('hourly', {})
+    times = hourly_data.requests.get('time', [])
+    heights = hourly_data.requests.get('swell_wave_height', [])
+    periods = hourly_data.requests.get('swell_wave_period', [])
 
     swell_data_list = []
 
     for time, height, period in zip(times, heights, periods):
         swell_data = SwellData(
-                    latitude=json_data.get('latitude'),
-                    longitude=json_data.get('longitude'),
-                    generationtime_ms=json_data.get('generationtime_ms'),
-                    utc_offset_seconds=json_data.get('utc_offset_seconds'),
-                    timezone=json_data.get('timezone'),
-                    timezone_abbreviation=json_data.get('timezone_abbreviation'),
-                    elevation=json_data.get('elevation'),
-                    hourly_units=json_data.get('hourly_units'),
+                    latitude=json_data.requests.get('latitude'),
+                    longitude=json_data.requests.get('longitude'),
+                    generationtime_ms=json_data.requests.get('generationtime_ms'),
+                    utc_offset_seconds=json_data.requests.get('utc_offset_seconds'),
+                    timezone=json_data.requests.get('timezone'),
+                    timezone_abbreviation=json_data.requests.get('timezone_abbreviation'),
+                    elevation=json_data.requests.get('elevation'),
+                    hourly_units=json_data.requests.get('hourly_units'),
                     hourly=time,
                     swell_wave_height=height,
                     swell_wave_period=period
@@ -55,7 +67,83 @@ def save_swell_data(parsed_data):
         db.close()
 
 
-# Functions for fetching and commiting grib files to database
+# Functions for fetching and commiting global grib wave forecast files from Wavewatch III models to database
 
-def fetch_grib_data
+def latest_url():
+    date = datetime.now().strftime("%Y%m%d")
+    url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{date}/00/wave/gridded/"
+    return url
+
+
+def get_grib2_links():    # parse the list of models
+    response = requests.get(latest_url())
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # match the average global model for all forecast hours
+    pattern = re.compile(r'gefs\.wave\.t00z\.mean\.global\.0p25\.f\d{3}\.grib2')
+    hrefs = [a.get('href') for a in soup.find_all('a', href=pattern)]
+    return hrefs
+
+
+def grib2_url_to_dataframe(target):
+    response = requests.get(f'{latest_url()}/{target}')
+    if response.status_code == 200:
+        # Use a temporary file to store the response content
+        with tempfile.NamedTemporaryFile() as tmp:
+            tmp.write(response.content)
+            tmp.flush()
+
+            # Open the dataset from the temporary file
+            with xr.open_dataset(tmp.name, engine='cfgrib') as ds:
+                # Extract the necessary data here
+                data = ds.load()  # 'load' will load the data into memory
+                # load to pandas dataframe
+                df = data.to_dataframe()
+                # drop landlocked rows
+                df = df.dropna(subset=['swh'])
+                # reset index
+                df.reset_index(level=['latitude', 'longitude'], inplace=True)
+
+                # Convert the timedelta to total number of hours as a string with ' hours' appended
+                df['step'] = df['step'].dt.total_seconds() / 3600.0
+                df['step'] = df['step'].astype(str) + ' hours'
+                return df
+                    
+    else:
+        print(f"Failed to get data: {response.status_code}")
+
+
+def save_dataframe_to_db(df, engine, table_name):
+    with engine.begin() as connection:  # Automatically handles transactions, including rollbacks if neccessary
+        try:
+            utc = pytz.utc
+            df['entry_updated'] = datetime.now(utc)
+            df.to_sql(table_name, con=connection, if_exists='append', index=False)
+            print(f"Successfully wrote grib2 file")
+        except SQLAlchemyError as e:
+            print(f"An error occurred: {e}")
+
+
+def all_wave_forecasts_to_db(engine, table_name):
+    count = 0
+    targets = get_grib2_links()
+    for target in targets:
+        df = grib2_url_to_dataframe(target)
+        save_dataframe_to_db(df, engine, table_name)
+        count += 1
+        print(f"Wrote grib file number {count} out of {len(targets)}")
+
+
+# debug area
+# from sqlalchemy import create_engine
+# table_name = 'wave_forecast'
+# DATABASE_URL = "postgresql://postgres:your_password@localhost:5432/surfing_data"
+# engine = create_engine(DATABASE_URL)
+
+# hrefs = get_grib2_links()
+# df = grib2_url_to_dataframe(hrefs[0])
+# print(df.head())
+
+# save_dataframe_to_db(df, engine, table_name)
+
+
 
--- backend/backend.dockerfile
+++ backend/backend.dockerfile
@@ -1,13 +1,27 @@
 # Use an official Python runtime as a parent image
 FROM python:3.10.12-slim
 
+
+# Install system dependencies required for psycopg2 and wget
+RUN apt-get update && apt-get install -y libpq-dev gcc wget && \
+    rm -rf /var/lib/apt/lists/*
+    
+# Install Miniconda
+ENV MINICONDA_VERSION=latest
+ENV CONDA_DIR=/opt/conda
+ENV PATH=$CONDA_DIR/bin:$PATH
+
+RUN wget https://repo.anaconda.com/miniconda/Miniconda3-$MINICONDA_VERSION-Linux-x86_64.sh \
+    && mkdir -p $CONDA_DIR \
+    && sh Miniconda3-$MINICONDA_VERSION-Linux-x86_64.sh -b -u -p $CONDA_DIR \
+    && rm -f Miniconda3-$MINICONDA_VERSION-Linux-x86_64.sh
+
+# Install cfgrib and eccodes using Conda
+RUN conda install -c conda-forge cfgrib
+
 # Install Poetry
 ENV POETRY_VERSION=1.7.0
 RUN pip install "poetry==$POETRY_VERSION"
-
-# Install system dependencies required for psycopg2
-RUN apt-get update && apt-get install -y libpq-dev gcc && \
-    rm -rf /var/lib/apt/lists/*
 
 # Set the working directory in the container
 WORKDIR /usr/src/app
