b153fdf35817f8855cc534fc993ffa491a73bfed
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-05 11:52:32 -0500

    testing threejs as react component

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import GetData from './components/GetData';
+import ThreeMap from './components/ThreeMap';
 
 function App() {
   return (
     <>
       <p>placeholder</p>
       <GetData />
+      <ThreeMap />
     </>
   );
 }
