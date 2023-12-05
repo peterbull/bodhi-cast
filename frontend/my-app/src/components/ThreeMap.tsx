b93695ed0bcaff9b780e5b96837a56df1c5cbcf7
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-05 11:58:08 -0500

    change to more react way of integrating threejs

--- frontend/my-app/src/components/ThreeMap.tsx
+++ frontend/my-app/src/components/ThreeMap.tsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const ThreeMap = () => {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+  const mountRef = useRef<any>(null);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  useEffect(() => {
+    if (!mountRef.current) {
+      return;
+    }
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
-  camera.position.z = 5;
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-  const animate = () => {
-    requestAnimationFrame(animate);
+    mountRef.current.appendChild(renderer.domElement);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-    renderer.render(scene, camera);
-  };
+    camera.position.z = 5;
 
-  animate();
+    const animate = () => {
+      requestAnimationFrame(animate);
 
-  return <div></div>;
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    // Clean up on unmount
+    return () => {
+      mountRef.current.removeChild(renderer.domElement);
+    };
+  }, []); // Empty array ensures effect is only run on mount and unmount
+
+  if (!mountRef.current) {
+    return <div>Loading...</div>;
+  }
+
+  return <div ref={mountRef}></div>;
 };
 
 export default ThreeMap;
