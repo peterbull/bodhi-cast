9ccb08758ec57415621a0bb777499a2346209a32
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-07 17:10:47 -0500

    add sample cli

--- backend/app/scripts/test_celery_tasks.py
+++ backend/app/scripts/test_celery_tasks.py
@@ -1,5 +1,5 @@
 import click
-from app.main import noaa_update
+from app.main import noaa_update, noaa_sample
 
 @click.group()
 def cli():
@@ -10,6 +10,13 @@ def cli():
 def run_noaa_update():
     """Run the NOAA update task."""
     noaa_update.delay()
+
+@cli.command()
+def run_noaa_sample():
+    """
+    Runs the NOAA sample task asynchronously using Celery.
+    """
+    noaa_sample.delay()
 
 if __name__ == "__main__":
     cli()
