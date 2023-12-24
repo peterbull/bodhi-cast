21175f1b27577ea840c21e6ef76b3ec26a0d9807
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-24 10:45:27 -0500

    add to spots

--- backend/app/data/spots/spots.py
+++ backend/app/data/spots/spots.py
@@ -3,4 +3,10 @@ spots = [{
     "longitude": -75.96764801341773,
     "spot_name": "First Street Jetty",
     "street_address": "100 2nd St, Virginia Beach, VA 23451"
+},
+{
+    "latitude": 33.37196277257346,
+    "longitude": -117.56709607562253,
+    "spot_name": "San Onofre State Beach",
+    "street_address": "Beach Club Rd, San Clemente, CA 92672"
 }]
