2c83127c243a80cb8c260bbcb84466a2f4a7aa15
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-25 06:22:46 -0500

    change default render

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,12 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
-//CSS and marker image fix for Leaflet map
-import "leaflet/dist/leaflet.css";
 import Globe from "react-globe.gl";
-import GlobeSwell from "./components/GlobeSwell";
-import SwellMap from "./components/SwellMap";
-import GlobeBump from "./components/GlobeBump";
 import GlobeSpots from "./components/GlobeSpots";
 
 export interface Coord {
@@ -25,7 +20,6 @@ export interface SwellData {
 function App() {
   const [swellData, setSwellData] = useState<SwellData[]>([]);
   const [spots, setSpots] = useState<any>([]);
-  const [currentComponent, setCurrentComponent] = useState<any>(null);
 
   useEffect(() => {
     const fetchSwell = async () => {
@@ -56,51 +50,7 @@ function App() {
     fetchAllSpots();
   }, []);
 
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setCurrentComponent("SwellMap");
-        }}
-      >
-        Show Swell Map
-      </button>
-      <button
-        onClick={() => {
-          setCurrentComponent("GlobeSwell");
-        }}
-      >
-        Show Globe Swell Map
-      </button>
-      <button
-        onClick={() => {
-          setCurrentComponent("GlobeBump");
-        }}
-      >
-        Show Globe Bump Map
-      </button>
-      <button
-        onClick={() => {
-          setCurrentComponent("GlobeSpots");
-        }}
-      >
-        Show Globe Spot Map
-      </button>
-
-      {swellData.length > 0 && currentComponent === "SwellMap" && (
-        <SwellMap swellData={swellData[0]} />
-      )}
-      {swellData.length > 0 && currentComponent === "GlobeSwell" && (
-        <GlobeSwell swellData={swellData[0]} />
-      )}
-      {swellData.length > 0 && currentComponent === "GlobeBump" && (
-        <GlobeBump swellData={swellData[0]} />
-      )}
-      {spots.length > 0 && currentComponent === "GlobeSpots" && (
-        <GlobeSpots spots={spots} />
-      )}
-    </div>
-  );
+  return <div>{spots.length > 0 && <GlobeSpots spots={spots} />}</div>;
 }
 
 export default App;
