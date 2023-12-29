8174b41c42e586f5b4233c868816222009ea566b
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-29 17:24:43 -0500

    add to spots

--- backend/app/data/spots/spots.py
+++ backend/app/data/spots/spots.py
@@ -15,4 +15,15 @@ spots = [{
     "longitude": -75.92244365210605,
     "spot_name": "Sandbridge Beach",
     "street_address": "3820 Sandpiper Rd, Virginia Beach, VA 23456"
+},
+    {
+    "latitude": 32.85755487389138,
+    "longitude": -117.26057679739844,
+    "spot_name": "La Jolla Shores",
+    "street_address": "8300 Camino Del Oro, La Jolla, CA 92037"
+}, {
+    "latitude": 34.42848355155381,
+    "longitude": -119.91944474969617,
+    "spot_name": "Haskell's Beach",
+    "street_address": "Hollister Ave, Goleta, CA 93117"
 }]
