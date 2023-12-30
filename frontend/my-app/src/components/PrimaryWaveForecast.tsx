b82efcbe3b7dc10560a08b2eda5c365bd0e30af1
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-30 13:42:16 -0500

    svg direction arrow added

--- frontend/my-app/src/components/PrimaryWaveForecast.tsx
+++ frontend/my-app/src/components/PrimaryWaveForecast.tsx
@@ -5,6 +5,8 @@ const PrimaryWaveForecast: React.FC<any> = ({ spotForecast, hourlyIndex }) => {
   const timeKey = "perpw";
   const directionKey = "dirpw";
 
+  const direction = parseFloat(spotForecast[hourlyIndex]["directionKey"]);
+
   return (
     <>
       <td>
@@ -13,7 +15,21 @@ const PrimaryWaveForecast: React.FC<any> = ({ spotForecast, hourlyIndex }) => {
       <td>ft</td>
       <td>{spotForecast[hourlyIndex][timeKey].toFixed(1)}</td>
       <td>s</td>
-      <td></td>
+      <td>
+        <svg
+          width="12"
+          height="18"
+          viewBox="0 0 8 13"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ transform: `rotate(${direction}deg)` }}
+        >
+          <path
+            d="M6.903 11.059 3.5 0 .097 11.059A1.5 1.5 0 0 0 1.531 13h3.938a1.5 1.5 0 0 0 1.434-1.941Z"
+            fill="#171717"
+          ></path>
+        </svg>
+      </td>
     </>
   );
 };
