1214a5b925e414c741aec35f2ec1127e3360a969
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-12-31 06:38:29 -0500

    remove old endpoints, lint, add docstrings

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,15 @@
-import json
 from datetime import datetime, timedelta
 
-from fastapi import FastAPI, Depends
-from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import extract, select, text, and_
-from sqlalchemy.orm import Session
-
 import redis
-
+from app.data.noaa.wavewatch import Wavewatch
+from app.db.database import add_spots, create_tables, engine, get_db
+from app.models.models import Spots
 from celery import Celery
 from celery.schedules import crontab
-
-from app.db.database import get_db, create_tables, engine, add_spots
-from app.data.noaa.wavewatch import Wavewatch
-from app.models.models import Spots, WaveForecast
-
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
+from sqlalchemy.orm import Session
 
 create_tables()
 db = get_db()
@@ -28,235 +23,141 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-redis_client = redis.Redis(host='redis', port=6379, db=0)
+# Configure redis
+redis_client = redis.Redis(host="redis", port=6379, db=0)
 
-celery_app = Celery(
-    "tasks",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0"
-)
+# Configure celery
+celery_app = Celery("tasks", broker="redis://redis:6379/0", backend="redis://redis:6379/0")
 
 celery_app.conf.broker_connection_retry_on_startup = True
 
+# Schedule celery tasks with celery-beat
 celery_app.conf.beat_schedule = {
-    'fetch-transform-commit-noaa-data-daily': {
-        'task': 'app.main.noaa_sample',
-        'args': (24,),
-        # Runs daily at 7am UTC 2am EST
-        'schedule': crontab(minute='0', hour='7'),
-        # 'schedule': crontab(minute='17', hour='19'), # Variable debug run
+    "fetch-transform-commit-noaa-data-daily": {
+        "task": "app.main.noaa_sample",
+        "args": (24,),
+        # Runs daily at 7:00 UTC 2:00am EST
+        "schedule": crontab(minute="0", hour="7"),
     }
 }
+
 # Add spots to spots table
 add_spots()
-# Celery tasks
+
+
+# Define celery tasks
 
 
 @celery_app.task
 def noaa_update():
-    Wavewatch(engine, 'wave_forecast').run()
+    """
+    Updates the Wavewatch data for wave_forecast.
+
+    This function fetches all available forecast periods, which forecast
+    out 16 days. This can be about 23 gigs of data so keep storage and compute in mind when running.
+    """
+    Wavewatch(engine, "wave_forecast").run()
 
 
 @celery_app.task
 def noaa_sample(num_samples=1):
-    Wavewatch(engine, 'wave_forecast').run_sample(num_samples=num_samples)
+    """
+    Updates the Wavewatch data for wave_forecast by number of samples.
 
+    Parameters:
+    num_samples (int): The number of forecast samples to fetch. Each sample represents a forecast
+    for a specific time interval. Default value is 1.
 
-@celery_app.task
-def noaa_update_swell_only():
-    Wavewatch(engine, 'wave_forecast').run(swell_only=True)
+    Details:
+    - For the first 10 days of a forecast, data is provided at 3-hour intervals.
+    (1 Day == 8 Samples)
+    - From day 11 to day 16, data is provided at 6-hour intervals.
+    (1 Day == 4 Samples)
 
-
-@celery_app.task
-def noaa_sample_swell_only():
-    Wavewatch(engine, 'wave_forecast').run_sample(swell_only=True)
+    """
+    Wavewatch(engine, "wave_forecast").run_sample(num_samples=num_samples)
 
 
 # Routes
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Surf"}
 
-
-@app.get("/waveforecast/{datatype}/{date}")
-def datatype_forecast_by_date(datatype: str, date: int, db: Session = Depends(get_db)):
-
-    def format_date(date_int: int) -> str:
-        date_obj = datetime.strptime(str(date_int), "%Y%m%d")
-        formatted_date = date_obj.strftime("%Y-%m-%d %H:%M:%S+00")
-        return formatted_date
-
-    sql_query = text(f"""
-    SELECT COALESCE({datatype}, 0) as {datatype}, latitude, longitude
-    FROM wave_forecast
-    WHERE valid_time = :date AND (latitude < 80 OR latitude > -80)
-    """)
-
-    result = db.execute(
-        sql_query, {"date": format_date(date)}).mappings().first()
-
-    if result is not None:
-        return result
-    else:
-        return {}
-
-
-@app.get("/locations/{date}")
-def get_locations(date: str, db: Session = Depends(get_db)):
-    date = datetime.strptime(date, "%Y%m%d").date()
-    result = db.execute(text(
-        """
-        SELECT ST_X(location::geometry) AS lon, ST_Y(location::geometry) AS
-        lat, swell FROM wave_forecast WHERE valid_time = :date;
-        """),
-        {"date": date})
-
-    rows = result.all()
-    locations = [row._asdict() for row in rows]
-
-    result = db.execute(text(
-        """
-        SELECT MAX(swell) AS max_swell FROM wave_forecast
-        WHERE valid_time = :date;
-        """),
-        {"date": date})
-
-    max_swell = result.scalar()
-
-    return {"locations": locations, "maxSwell": max_swell}
-
-
-@app.get("/locations/gridded/{degrees}/{date}")
-def get_locations_gridded(date: str, degrees: str, db: Session = Depends(get_db)):
-    date = datetime.strptime(date, "%Y%m%d").date()
-    result = db.execute(text(
-        """
-        SELECT
-            ST_X(ST_SnapToGrid(location::geometry, :degrees)) AS lon,
-            ST_Y(ST_SnapToGrid(location::geometry, :degrees)) AS lat,
-            AVG(swell) as avg_swell
-        FROM wave_forecast WHERE valid_time = :date GROUP BY
-        ST_SnapToGrid(location::geometry, :degrees);
-        """),
-        {"date": date, "degrees": int(degrees)})
-
-    rows = result.all()
-    locations = [row._asdict() for row in rows]
-
-    result = db.execute(text(
-        """
-        SELECT MAX(swell) AS max_swell FROM wave_forecast
-        WHERE valid_time = :date;
-        """),
-        {"date": date})
-
-    max_swell = result.scalar()
-
-    return {"locations": locations, "maxSwell": max_swell}
-
-
-@app.get("/forecasts/gridded/{degrees}/{date}")
-def get_forecasts_gridded(date: str, degrees: str, db: Session = Depends(get_db)):
-    cache_key = f"forecasts_gridded:{date}:{degrees}"
-    cached_result = redis_client.get(cache_key)
-
-    if cached_result is not None:
-        return json.loads(cached_result.decode("utf-8"))
-    else:
-        date = datetime.strptime(date, "%Y%m%d").date()
-        result = db.execute(text(
-            """
-            SELECT
-                ST_X(ST_SnapToGrid(location::geometry, :degrees)) AS lon,
-                ST_Y(ST_SnapToGrid(location::geometry, :degrees)) AS lat,
-                valid_time,
-                step,
-                AVG(swell) as avg_swell
-            FROM wave_forecast
-            WHERE valid_time >= :date
-            GROUP BY ST_SnapToGrid(location::geometry, :degrees), valid_time, step
-            ORDER BY ST_SnapToGrid(location::geometry, :degrees), valid_time;
-            """),
-            {"date": date, "degrees": int(degrees)})
-
-        rows = result.all()
-        # Group forecasts by valid_time
-        forecasts_by_time = []
-        for row in rows:
-            valid_time_str = row.valid_time.isoformat()
-            forecast_data = next(
-                (item for item in forecasts_by_time if item["time"] == valid_time_str), None)
-            if not forecast_data:
-                forecast_data = {
-                    "time": valid_time_str,
-                    "step": row.step.total_seconds(),
-                    "maxSwell": row.avg_swell or 0,
-                    "locations": []
-                }
-                forecasts_by_time.append(forecast_data)
-            forecast = {"lon": row.lon, "lat": row.lat, "swell": row.avg_swell}
-            forecast_data["locations"].append(forecast)
-            if row.avg_swell is not None:
-                forecast_data["maxSwell"] = max(
-                    forecast_data["maxSwell"] or 0, row.avg_swell)
-
-        redis_client.set(cache_key, json.dumps(forecasts_by_time), ex=86400)
-
-        return forecasts_by_time
-
-
-@app.get("/forecasts/swell")
-def get_swell_forecasts(db: Session = Depends(get_db)):
-    result = db.execute(text(
-        """
-        SELECT
-            latitude, longitude, valid_time, step, swell
-        FROM wave_forecast
-        WHERE swell IS NOT NULL
-        ORDER BY valid_time;
-        """
-    ))
-    rows = result.all()
-    return [row._asdict for row in rows]
 
 # Get wave forecast data if points fall within a given bounding box
 
 
 @app.get("/forecasts/tiles/{date}/{lat}/{lng}/{zoom}")
 def get_forecasts_by_tile(date: str, lat: str, lng: str, zoom: str, db: Session = Depends(get_db)):
+    """
+    Retrieve wave forecasts for a specific tile based on date, latitude, longitude, and zoom level.
+
+    PostGIS is used to create a bounding box at calculated offets based on zoom from the orign
+    point(lat,lng) and return all data points that fall within the bounding box.
+
+    Args:
+        date (str): The date of the forecasts in the format 'YYYYMMDD'.
+        lat (str): The latitude of the tile.
+        lng (str): The longitude of the tile.
+        zoom (str): The zoom level of the tile.
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        list: A list of dictionaries representing the wave forecasts for the tile.
+    """
     date = datetime.strptime(date, "%Y%m%d").date()
-    # To do: Find an equation that returns proportionally at all zoom levels
-    # Current: A placeholder that will only really work with values near the default `zoom`
+    # To do: Find an equation that returns proportionally at all zoom levels after deciding on
+    #        final map and corresponding projection and pixel size
+    # Current: A placeholder that will only work with values near the default `zoom`
     zoom_factor = float(zoom) / 7.5
     lat_min = float(lat) - (zoom_factor / 2)
     lat_max = float(lat) + (zoom_factor / 2)
     lng_min = float(lng) - zoom_factor
     lng_max = float(lng) + zoom_factor
-    result = db.execute(text(
-        """SELECT *
+    result = db.execute(
+        text(
+            """SELECT *
         FROM wave_forecast
         WHERE
             location::geometry && ST_MakeEnvelope(:lng_min, :lat_min, :lng_max, :lat_max, 4326)
             AND ST_Intersects(
                 location::geometry,
                 ST_MakeEnvelope(:lng_min, :lat_min, :lng_max, :lat_max, 4326));
-                """),
-        {"lng_min": lng_min, "lat_min": lat_min, "lng_max": lng_max, "lat_max": lat_max})
+                """
+        ),
+        {"lng_min": lng_min, "lat_min": lat_min, "lng_max": lng_max, "lat_max": lat_max},
+    )
     rows = result.all()
     return [row._asdict() for row in rows]
-
-# Find the nearest point in the db to the spot, then return all forecast items for that day
-# Swell is null at landlocked areas, so excluding it ensures the selected point is at sea
 
 
 @app.get("/forecasts/spots/{date}/{spot_lat}/{spot_lng}/")
 def get_forecasts_by_spot(date: str, spot_lat: str, spot_lng: str, db: Session = Depends(get_db)):
+    """
+    Retrieve wave forecasts for a specific spot based on date and coordinates.
+
+    - This function creates a PostGIS point using the provided latitude (spot_lat) and longitude
+      (spot_lng) as the origin.
+    - It then calculates the nearest data point by distance to this origin.
+    - Forecasts where significant combined swell and wind wave height (swh) values
+      are null are excluded, as null values indicate land areas rather than water.
+
+    Args:
+        date (str): The date in the format 'YYYYMMDD'.
+        spot_lat (str): The latitude of the spot.
+        spot_lng (str): The longitude of the spot.
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        list: A list of dictionaries containing the forecast data for each valid time.
+    """
     date = datetime.strptime(date, "%Y%m%d").date()
     next_day = date + timedelta(days=1)
     spot_lat = float(spot_lat)
     spot_lng = float(spot_lng)
 
-    sql = text("""
+    sql = text(
+        """
         WITH closest_point AS (
             SELECT latitude, longitude
             FROM wave_forecast
@@ -282,30 +183,45 @@ def get_forecasts_by_spot(date: str, spot_lat: str, spot_lng: str, db: Session =
             AND latitude = (SELECT latitude FROM closest_point)
             AND longitude = (SELECT longitude FROM closest_point)
         ORDER BY valid_time;
-    """)
+    """
+    )
 
-    result = db.execute(sql, {"date": date, "next_day": next_day,
-                        "spot_lat": spot_lat, "spot_lng": spot_lng})
+    result = db.execute(
+        sql, {"date": date, "next_day": next_day, "spot_lat": spot_lat, "spot_lng": spot_lng}
+    )
 
     rows = result.all()
     return [row._asdict() for row in rows]
+
 
 # Get all spots
 
 
 @app.get("/spots")
 def get_all_spots(db: Session = Depends(get_db)):
+    """
+    Retrieve all spots from the database.
+
+    Parameters:
+    - db: The database session.
+
+    Returns:
+    - A list of dictionaries representing each spot.
+    """
     spots = db.query(Spots).all()
     return [spot.as_dict() for spot in spots]
+
 
 # Celery Worker Status
 
 
 @app.get("/worker-status")
 def get_worker_status():
+    """
+    Retrieve the status of the worker.
+
+    Returns:
+        dict: A dictionary containing the number of active, scheduled, and reserved tasks.
+    """
     i = celery_app.control.inspect()
-    return {
-        "active": i.active(),
-        "scheduled": i.scheduled(),
-        "reserved": i.reserved()
-    }
+    return {"active": i.active(), "scheduled": i.scheduled(), "reserved": i.reserved()}
