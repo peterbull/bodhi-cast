23c09e0882a6760db00fee82731e175cbdb9ec3e
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-01-05 13:41:34 -0500

    update for production settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Restrict to frontend
+    allow_origins=["http://localhost"],  # Restrict to frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
