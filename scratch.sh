a980583fe298c52381a37454217b3717fb19967c
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-03 05:35:13 -0500

    update redis and postgres config to env variables

--- scratch.sh
+++ scratch.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Load environment variables from .env file
+set -a # automatically export all variables
+source .env
+set +a # stop automatically exporting variables
 
 docker exec bodhi-cast-airflow-worker-1 airflow dags trigger gefs_wave_urls_to_kafka && \
 docker exec bodhi-cast-airflow-worker-1 airflow dags trigger gefs_wave_etl_from_kafka
 
-docker exec -it bodhi-cast-postgres-1 psql -U your_postgres_user -d your_database -c "SELECT COUNT(*) FROM wave_forecast;"
+sleep 150
 
+docker exec bodhi-cast-postgres-1 psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c "SELECT COUNT(*) FROM wave_forecast;"
