09c8174782ff803a1587d7fb66a5d5bbe85a1497
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-03 11:07:12 -0500

    Add SQL query to count rows in wave_forecast table

--- scratch.sh
+++ scratch.sh
@@ -4,6 +4,8 @@ set -a # automatically export all variables
 source .env
 set +a # stop automatically exporting variables
 
+docker exec bodhi-cast-postgres-1 psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c "SELECT COUNT(*) FROM wave_forecast;"
+
 docker exec bodhi-cast-airflow-worker-1 airflow dags trigger gefs_wave_urls_to_kafka && \
 docker exec bodhi-cast-airflow-worker-1 airflow dags trigger gefs_wave_etl_from_kafka
 
