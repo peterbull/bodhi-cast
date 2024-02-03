8840d60dce58726a06faf18762ea3ea41767fe8b
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-03 04:22:12 -0500

    update startup scripts and env

--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-# Create .env file with info for permissions
-echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+# Get the new UID and GID
+new_uid=$(id -u)
+new_gid=0
+
+# Check if AIRFLOW_UID exists in the .env file
+if grep -q "AIRFLOW_UID" .env; then
+    # If it exists, update it
+    sed -i "s/^AIRFLOW_UID=.*/AIRFLOW_UID=$new_uid/" .env
+else
+    # If it doesn't exist, append it
+    echo "AIRFLOW_UID=$new_uid" >> .env
+fi
+
+# Check if AIRFLOW_GID exists in the .env file
+if grep -q "AIRFLOW_GID" .env; then
+    # If it exists, update it
+    sed -i "s/^AIRFLOW_GID=.*/AIRFLOW_GID=$new_gid/" .env
+else
+    # If it doesn't exist, append it
+    echo "AIRFLOW_GID=$new_gid" >> .env
+fi
