2c9b2347d52a3321a1279c1953fdd4c1c1377680
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-04 14:08:10 -0500

    Refactor GlobeSpots component and add nearby spots section

--- frontend/my-app/src/components/GlobeSpots.tsx
+++ frontend/my-app/src/components/GlobeSpots.tsx
@@ -44,49 +44,60 @@ const GlobeSpots: React.FC<any> = ({
 
   return spots.length > 0 ? (
     <>
-      <div className="bg-gray-900">
-        <div>
-          <Globe
-            ref={globeEl}
-            globeImageUrl={globeImageUrl}
-            backgroundColor="rgb(17 24 39)"
-            labelsData={spots}
-            labelLat="latitude"
-            labelLng="longitude"
-            labelText="spot_name"
-            labelSize={0.0}
-            labelDotRadius={0.4}
-            labelColor={() => "rgba(164, 255, 61, 0.5)"}
-            labelLabel={(spot: any) =>
-              `<div>
+      <div className="flex flex-col">
+        <div className="bg-gray-900 overflow-x-hidden">
+          <div>
+            <Globe
+              ref={globeEl}
+              height={700}
+              globeImageUrl={globeImageUrl}
+              backgroundColor="rgb(17 24 39)"
+              labelsData={spots}
+              labelLat="latitude"
+              labelLng="longitude"
+              labelText="spot_name"
+              labelSize={0.0}
+              labelDotRadius={0.4}
+              labelColor={() => "rgba(164, 255, 61, 0.5)"}
+              labelLabel={(spot: any) =>
+                `<div>
           <b>${spot.spot_name}</b>
         </div>`
-            }
-            onGlobeClick={({ lat, lng }: any) => {
-              console.log(`Clicked at latitude: ${lat}, longitude: ${lng}`);
-              setSpotClick([lat, lng]);
-            }}
-            onLabelClick={(label: any) => {
-              globeEl.current.pointOfView(
-                {
-                  lat: label.latitude,
-                  lng: label.longitude,
-                  altitude: 0.1,
-                },
-                2500
-              );
-              setCurrentSpot(spots.find((spot: any) => spot.id === label.id));
-              setTimeout(() => {
-                setCurrentComponent("SwellMap");
-              }, 2500);
-            }}
-          />
+              }
+              onGlobeClick={({ lat, lng }: any) => {
+                console.log(`Clicked at latitude: ${lat}, longitude: ${lng}`);
+                setSpotClick([lat, lng]);
+              }}
+              onLabelClick={(label: any) => {
+                globeEl.current.pointOfView(
+                  {
+                    lat: label.latitude,
+                    lng: label.longitude,
+                    altitude: 0.1,
+                  },
+                  2500
+                );
+                setCurrentSpot(spots.find((spot: any) => spot.id === label.id));
+                setTimeout(() => {
+                  setCurrentComponent("SwellMap");
+                }, 2500);
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <h1 className="text-[#03e9f4] text-3xl text-thin text-left">
-          Nearby Spots
-        </h1>
+        <div className="flex">
+          <div className="flex-auto w-2/3">
+            <h1 className="text-[#03e9f4] text-3xl text-thin text-center">
+              Nearby Spots
+            </h1>
+          </div>
+          <div className="flex-auto w-1/3">
+            <button className="text-[#03e9f4] uppercase-tracking-[4px] border-2 border-[#03e9f4] rounded px-6 py-2">
+              ADD A SPOT
+            </button>
+            Add A Spot
+          </div>
+        </div>
       </div>
     </>
   ) : (
