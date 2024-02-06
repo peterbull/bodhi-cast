c1994ba46e93a45cd96ee6d319fab8b561bd5b5a
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-06 06:49:39 -0500

    add debug files for postgres issue

---
+++ airflow/dags/debug_gefs_wave_urls_to_kafka.py
@@ -0,0 +1,124 @@
+import logging
+import os
+import re
+from urllib.parse import urljoin
+
+import pendulum
+import requests
+from airflow.decorators import task
+from bs4 import BeautifulSoup
+from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient, ConfigResource
+
+from airflow import DAG
+
+sasl_username = os.environ.get("KAFKA_DEFAULT_USERS")
+sasl_password = os.environ.get("KAFKA_DEFAULT_PASSWORDS")
+
+start_date = pendulum.datetime(2024, 1, 1)
+
+topic = "gefs_wave_urls"
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": start_date,
+    "email": ["your-email@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": pendulum.duration(minutes=5),
+}
+
+
+@task
+def get_gefs_wave_urls(epoch, date):
+    """
+    Retrieves the URLs of mean ensemble wave models for a given epoch and date.
+
+    Args:
+        epoch (str): The epoch(hour) of the wave models.
+        date (str): The date of the wave models.
+
+    Returns:
+        list: A list of URLs of mean ensemble wave models.
+    """
+    url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{date}/{epoch}/wave/gridded/"
+    # Pattern for mean ensemble wave models at epoch:
+    pattern = re.compile(r".*\.mean\.global\.0p25\.f\d{3}\.grib2")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    urls = [urljoin(url, a.get("href")) for a in soup.find_all("a", href=pattern)]
+
+    return urls
+
+
+@task
+def send_urls_to_kafka(urls, topic, sasl_username=sasl_username, sasl_password=sasl_password):
+    conf = {
+        "bootstrap.servers": "kafka:9092",
+        "security.protocol": "SASL_PLAINTEXT",
+        "sasl.mechanisms": "PLAIN",
+        "sasl.username": sasl_username,
+        "sasl.password": sasl_password,
+    }
+
+    producer = Producer(conf)
+
+    delivery_reports = []
+
+    """Create and check delivery report
+    Airflow will otherwise always return `success` at the end of the producer flush, 
+    even if it fails to write"""
+
+    def delivery_report(err, msg):
+        if err is not None:
+            delivery_reports.append((False, f"Message delivery failed: {err}"))
+            logging.error(f"Message delivery failed: {err}")
+        else:
+            delivery_reports.append(
+                (True, f"Message delivered to {msg.topic()} [{msg.partition()}]")
+            )
+            logging.info(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+
+    for url in urls:
+        try:
+            producer.produce(topic, url, callback=delivery_report)
+        except Exception as e:
+            logging.error(f"Failed to send message to Kafka: {e}")
+
+    try:
+        producer.flush()
+    except Exception as e:
+        logging.error(f"Failed to flush messages to Kafka: {e}")
+
+    for success, report in delivery_reports:
+        if not success:
+            logging.error(report)
+
+    if all(success for success, report in delivery_reports):
+        logging.info("All messages were successfully delivered to Kafka.")
+    else:
+        logging.error("Some messages failed to be delivered to Kafka.")
+
+
+# revisit to refactor based on https://airflow.apache.org/docs/apache-airflow/2.8.1/best-practices.html#top-level-python-code
+with DAG(
+    "gefs_wave_urls_to_kafka",
+    default_args=default_args,
+    description="Get GEFS wave forecast grib2 file urls",
+    schedule_interval="*/5 * * * *",
+    catchup=False,
+) as dag:
+    # Available forecasts
+    forecast_intervals = ["00", "06", "12", "18"]
+    # Fetch just the first model of the day due to storage size on disk
+    epoch = forecast_intervals[0]
+    date = pendulum.now("UTC").strftime("%Y%m%d")  # Current Time UTC
+    gefs_wave_urls = get_gefs_wave_urls(epoch, date)
+    send_to_kafka = send_urls_to_kafka(
+        gefs_wave_urls, topic=topic, sasl_username=sasl_username, sasl_password=sasl_password
+    )
+
+if __name__ == "__main__":
+    dag.test()
---
+++ airflow/dags/debug_wave_etl_kafka_consumption.py
@@ -0,0 +1,128 @@
+import logging
+import os
+import re
+import tempfile
+
+import pandas as pd
+import pendulum
+import requests
+import xarray as xr
+from airflow.decorators import task
+from confluent_kafka import Consumer, KafkaException
+from geoalchemy2 import WKTElement
+from geoalchemy2.types import Geography
+from shapely.geometry import Point
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+
+from airflow import DAG
+
+sasl_username = os.environ.get("KAFKA_DEFAULT_USERS")
+sasl_password = os.environ.get("KAFKA_DEFAULT_PASSWORDS")
+
+DATABASE_URL = os.environ.get("AIRFLOW__DATABASE__SQL_ALCHEMY_CONN")
+engine = create_engine(DATABASE_URL)
+
+table_name = "wave_forecast"
+topic = "gefs_wave_urls"
+
+start_date = pendulum.datetime(2024, 1, 1)
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": start_date,
+    "email": ["your-email@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": pendulum.duration(minutes=5),
+}
+
+
+# Will need to revisit this in the future. This is very basic fault handling,
+# where a single url runs through at a time, such that if there is a failure,
+# it will not be committed to the offset and a retry will resume at the correct message
+@task
+def consume_from_kafka(
+    topic, engine, table_name, bs=1, sasl_username=sasl_username, sasl_password=sasl_password
+):
+    """
+    Consume messages from a Kafka topic.
+
+    Args:
+        topic (str): The name of the Kafka topic to consume from.
+        bs (int, optional): The batch size of messages to consume at once. Defaults to 1.
+
+    Returns:
+        list: A list of consumed messages.
+
+    Raises:
+        KafkaException: If there is an error consuming from the Kafka topic.
+    """
+    conf = {
+        "bootstrap.servers": "kafka:9092",
+        "group.id": "airflow-consumers",
+        "enable.auto.commit": False,
+        "auto.offset.reset": "earliest",  # consume from the start of topic
+        "security.protocol": "SASL_PLAINTEXT",
+        "sasl.mechanisms": "PLAIN",
+        "sasl.username": sasl_username,
+        "sasl.password": sasl_password,
+    }
+
+    c = Consumer(conf)
+
+    c.subscribe([topic])
+    try:
+        pattern = re.compile(
+            r"https://nomads\.ncep\.noaa\.gov/pub/data/nccf/com/gens/prod/gefs\.(\d{8})/00/"
+        )
+
+        processed_count = 0
+        while True:
+            msg = c.poll(9.0)
+            if msg is None:
+                logging.info(f"No more messages in topic {topic}")
+                break
+            if msg.error():
+                logging.error(f"Error consuming from topic {topic}: {msg.error()}")
+                raise KafkaException(msg.error())
+            else:
+                message = msg.value().decode("utf-8")
+                match = pattern.match(message)
+                date = match.group(1)
+                current_date = pendulum.now().format("YYYYMMDD")
+                if date == current_date and processed_count < bs:
+                    logging.info(f"Beginning processing of {message}")
+                    # Commit the offset after successful processing
+                    c.commit()
+                    logging.info(f"Consumed and processed message from {topic}")
+                    processed_count += 1
+                else:
+                    c.commit()
+                    logging.info(f"Skipping processing and updating offset for: {message}")
+
+    finally:
+        c.close()
+        logging.info("Consumer closed")
+
+
+with DAG(
+    "gefs_wave_etl_from_kafka",
+    default_args=default_args,
+    description="Get GEFS grib2 urls from topic and batch process to postgis",
+    schedule_interval="*/6 * * * *",
+    catchup=False,
+) as dag:
+    data = consume_from_kafka(
+        topic=topic,
+        engine=engine,
+        table_name=table_name,
+        bs=8,
+        sasl_username=sasl_username,
+        sasl_password=sasl_password,
+    )
+
+if __name__ == "__main__":
+    dag.test()
