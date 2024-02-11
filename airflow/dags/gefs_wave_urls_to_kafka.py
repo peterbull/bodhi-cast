68e37de59eb48865bf630b3d59b22083e9159330
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-11 08:59:12 -0500

    update default retries

--- airflow/dags/gefs_wave_urls_to_kafka.py
+++ airflow/dags/gefs_wave_urls_to_kafka.py
@@ -26,7 +26,7 @@ default_args = {
     "email": ["your-email@example.com"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 5,
     "retry_delay": pendulum.duration(minutes=5),
 }
 
