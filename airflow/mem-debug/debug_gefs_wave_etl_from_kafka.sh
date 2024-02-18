c54ccc67aaac79aa36458382d266598129c46a4f
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-18 10:27:01 -0500

    add script to automate flamegraph generation

--- airflow/mem-debug/debug_gefs_wave_etl_from_kafka.sh
+++ airflow/mem-debug/debug_gefs_wave_etl_from_kafka.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
+# Setup for running memray on airflow dags to test memory usage and leaks
+
+# This script is used to set up and run memray on Airflow DAGs in order to test memory usage and leaks. 
+# It prompts the user to change the values of the MEM_DAG and MEM_TASK environment variables, and then runs the memray command to analyze memory usage.
+# The output is saved in a binary file and a flamegraph HTML file is generated for visualization.
 
 DESIRED_CWD="mem-debug"
 
+echo "Current MEM_DAG: $MEM_DAG"
+echo "Current MEM_TASK: $MEM_TASK"
+
+# Prompt the user to change the values of MEM_DAG and MEM_TASK
+read -p "Would you like to change the MEM_DAG and MEM_TASK values? (y/N) " change
+
+if [ "$change" = "y" ]; then
+    echo "Enter the new MEM_DAG value:"
+    read MEM_DAG
+    export MEM_DAG
+    echo "Enter the new MEM_TASK value:"
+    read MEM_TASK
+    export MEM_TASK
+fi
+
+echo "new MEM_DAG: $MEM_DAG"
+echo "new MEM_TASK: $MEM_TASK"
+echo "Current MEM_SLEEP: $MEM_SLEEP"
+
+read -p "Would you like to change the MEM_SLEEP values? (y/N) " change
+if [ "$change" = "y" ]; then
+    echo "Enter the new MEM_SLEEP value:"
+    read MEM_SLEEP
+    export MEM_SLEEP
+fi
+
+echo "new MEM_SLEEP: $MEM_SLEEP"
+
+# Check if MEM_DAG and MEM_TASK are set
+if [ -z "$MEM_DAG" ] || [ -z "$MEM_TASK" ] || [ -z "$MEM_SLEEP" ]; then
+    echo "Error: The MEM_DAG, MEM_TASK, MEM_SLEEP environment variables must be set."
+    exit 1
+fi
 
 CURRENT_DIR=$(basename "$(pwd)")
 
@@ -11,21 +49,31 @@ if [ "$CURRENT_DIR" != "$DESIRED_CWD" ]; then
     exit 1
 fi
 
-OUTPUT_DIR="./memray-bin"
+OUTPUT_DIR="memray-bin"
 timestamp=$(date +%Y%d%H%M%S)
 
 mkdir -p "$OUTPUT_DIR"
 echo "Please enter the desired output file name (without extension):"
 read output_file_name
 
+if [ -z "$output_file_name" ]; then
+    output_file_name="anonymous"
+fi
+
 output_file_name="${timestamp}_${output_file_name}"
 
-airflow dags trigger gefs_wave_urls_to_kafka # add new urls to kafka stream
-sleep 30 # wait for stream to populate
+# Trigger the Airflow DAG to add new URLs to the Kafka stream
+airflow dags trigger gefs_wave_urls_to_kafka
 
-memray run -o "./$OUTPUT_DIR/${output_file_name}.bin" -m airflow tasks test monitor_kafka_topics monitor_kafka_topic
+# Wait for the stream to populate
+sleep $MEM_SLEEP
 
-OUTPUT_DIR="./memray-html"
-mkdir -p "$OUTPUT_DIR"
+# Run memray command to analyze memory usage
+memray run -o "$OUTPUT_DIR/${output_file_name}.bin" -m airflow tasks test $MEM_DAG $MEM_TASK 
 
-/usr/local/bin/python -m memray flamegraph "./$OUTPUT_DIR/${output_file_name}.bin"
+HTML_DIR="./memray-html"
+mkdir -p "$HTML_DIR"
+
+# Generate flamegraph HTML file for visualization
+/usr/local/bin/python -m memray flamegraph "$OUTPUT_DIR/${output_file_name}.bin"
+mv "$OUTPUT_DIR/memray-flamegraph-${output_file_name}.html" "./memray-html/${output_file_name}-memray-flamegraph.html"
