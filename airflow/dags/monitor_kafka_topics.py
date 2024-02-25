e1c07c49b1ed41ad14cf7fde8bea39e865cfbd0e
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-02-25 06:49:16 -0500

    add total messages output to kafka monitor

--- airflow/dags/monitor_kafka_topics.py
+++ airflow/dags/monitor_kafka_topics.py
@@ -52,6 +52,7 @@ with DAG(
 
             c.subscribe([topic])
 
+            total_msgs = 0
             while True:
                 msg = c.poll(9.0)
                 if msg is None:
@@ -61,8 +62,10 @@ with DAG(
                     logging.error(f"Error consuming from topic {topic}: {msg.error()}")
                     raise KafkaException(msg.error())
                 logging.info("Received message: {}".format(msg.value().decode("utf-8")))
+                total_msgs += 1
             # Skipping committing the offsets so that this can be run as many times as needed
             c.close()
+            logging.info(f"Total messages: {total_msgs}")
 
         monitor_wave_urls = monitor_kafka_topic(
             topic="gefs_wave_urls", sasl_username=sasl_username, sasl_password=sasl_password
