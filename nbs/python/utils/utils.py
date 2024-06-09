ba8e1d69acefbdcf9cf7b63688ee06688ffce677
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-06-09 18:38:35 -0400

    nb flatten wave json data and meta

--- nbs/python/utils/utils.py
+++ nbs/python/utils/utils.py
@@ -1,1 +1,2 @@
-LOCAL_DATBASE_URL = "postgresql+psycopg2://airflow:airflow@localhost:5432/airflow"
+LOCAL_AIRFLOW_PG_URI = "postgresql+psycopg2://airflow:airflow@localhost:5432/airflow"
+LOCAL_PG_URI = "postgresql+psycopg2://airflow:airflow@localhost:5432/postgres"
