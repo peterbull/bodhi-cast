1231a4cac912fac8f0a550bed7a14522ffc0ef84
peter bull <petercbull@gmail.com>
2025-01-23 06:39:26 -0500

    sequential processing

--- typeflow/src/index.ts
+++ typeflow/src/index.ts
@@ -1,13 +1,29 @@
 import { updateWaveMeasurements } from "./db/queries";
 import { getMeanGlobalForecastUrls } from "./utils";
 import { EccodesWrapper } from "eccodes-ts";
+
+function createWrapper(url: string) {
+  return new EccodesWrapper(url);
+}
+
+async function updateSwellWaveHeight(url: string) {
+  const wrapper = createWrapper(url);
+  const swh = await wrapper.getSignificantWaveHeight({ addLatLon: true });
+  await updateWaveMeasurements(swh);
+}
+
+async function updatePrimaryWavePeriod(url: string) {
+  const wrapper = createWrapper(url);
+  const perpw = await wrapper.getPrimaryWavePeriod({ addLatLon: true });
+  await updateWaveMeasurements(perpw);
+}
+
 async function main() {
   const links = await getMeanGlobalForecastUrls();
-  const wrapper = new EccodesWrapper(links[0]); // just get first link for now
-  console.log("pause");
-  const swh = await wrapper.getSignificantWaveHeight({ addLatLon: true });
-  console.log("swh", swh);
-  await updateWaveMeasurements(swh);
+  const singleLink = links[0];
+
+  await updateSwellWaveHeight(singleLink);
+  await updatePrimaryWavePeriod(singleLink);
   return links;
 }
 
